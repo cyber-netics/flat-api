d3ffdf90349358ba1dfd522e5265b1479b53bf20
Eduard  Jacobs <eduardjacobs@Eduards-MacBook-Pro.local>
2021-03-28 06:01:48 -0700

    test

--- src/backend/src/routes/index.ts
+++ src/backend/src/routes/index.ts
@@ -3,7 +3,7 @@ import User from "../controllers";
 
 const router = Router();
 
-router.get("/getusers", User.getAllUsers);
+router.get("/", User.getAllUsers);
 router.post("/createuser", User.createUser);
 
 export default router;
